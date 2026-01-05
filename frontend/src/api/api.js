5a1eb277387e379deb4d78f7fd3dd91acd7fe532
amit2003dh <amit2003dh@gmail.com>
2026-01-05 12:15:00 +0530

    ds

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://kisansetu-2.onrender.com",
+  baseURL: process.env.REACT_APP_API_URL+"/api" || "http://localhost:5000",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
